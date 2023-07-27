2b531629b88d11817abf7c5b7e94c6f478d739ae
chrischen <chrischen96@outlook.com>
2023-07-26 19:13:04 -0700

    whitenoise

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -55,11 +55,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://appname.herokuapp.com',
-    'https://railway.app',
+    'https://mygallery-production.up.railway.app',
 ]
 
 ROOT_URLCONF = 'mygallery.urls'
@@ -132,8 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
