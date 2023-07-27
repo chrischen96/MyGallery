6a8eb71e9c4bf8d02f179bbc19791fd5f4c5aa17
chrischen <chrischen96@outlook.com>
2023-07-27 06:26:17 -0700

    register newuser

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -24,8 +24,9 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', include('gallery.urls')),
+    path('', include('gallery.urls'), name='gallery'),
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls'), name='users'),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
